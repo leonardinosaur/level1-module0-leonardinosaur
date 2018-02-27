021c5e48d180c2f5a225895f6bb8b96a411e297d
leonardinosaur <leonardinodigma@ucsd.edu>
2018-02-27 00:10:51 -0800

    level1 init1

--- src/MyFirstSwingGUI.java
+++ src/MyFirstSwingGUI.java
@@ -17,7 +17,7 @@ public class MyFirstSwingGUI {
 		//1. Create and initialize an object of the JFrame class
 		JFrame frame = new JFrame();
 		//2. Set your JFrame object to be visible
-
+		frame.setVisible(true);
 		//3. Run your program. Do you see your window? It's probably very small.
 		
 		//4. Set the default close operation to JFrame.EXIT_ON_CLOSE
